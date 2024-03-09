e4d251543cf2dcc567f182d91a02bdef9926989e
hajduty <hajduty@outlook.com>
2024-03-09 17:09:20 +0100

    add tailwindcss & remove bootstrap

---
+++ Notes/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Pages/**/*.{razor,html,cshtml}", "./Shared/**/*.{razor,html,cshtml}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
