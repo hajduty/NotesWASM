0df4ca234598b00430e092855636ec2b7cfa7585
hajduty <hajduty@outlook.com>
2024-03-12 21:52:11 +0100

    Add md.cs & mdservice.cs

--- NotesWASM/Data/Md.cs
+++ NotesWASM/Data/Md.cs
@@ -1,6 +1,14 @@
 ﻿namespace NotesWASM.Model
 {
-    public class Md
+    public class Moody
     {
+        public MoodyData ActiveNote { get; set; } = new();
+    }
+
+    public class MoodyData
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Text { get; set; } = "# hello world";
+        public string Title { get; set; } = "Untitled";
     }
 }
