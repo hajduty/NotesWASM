b7ad81c634efe66d266e65fac142d96d38d6b5b0
hajduty <hajduty@outlook.com>
2024-03-12 22:38:06 +0100

    Add files to wasm project

--- NotesWASM/Program.cs
+++ NotesWASM/Program.cs
@@ -1,11 +1,15 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NotesWASM;
+using NotesWASM.Model;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new MoodyService());
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
